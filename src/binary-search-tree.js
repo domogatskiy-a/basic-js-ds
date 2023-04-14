637de35f70744a580532f415a745e061cef50624
Домогатский Алексей <domogatskiy.a@yandex.ru>
2023-04-14 17:14:10 +0300

    feat: complit task

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -1,4 +1,5 @@
 const { NotImplementedError } = require('../extensions/index.js');
+const { Node } = require('../extensions/list-tree.js');
 
 // const { Node } = require('../extensions/list-tree.js');
 
@@ -7,15 +8,47 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
-
-  root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this._root = null;
+    this.data = null;
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  root() {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    return this._root
+  }
+
+  add( data ) {
+    //throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    console.log("ADD - data:", data)
+    const addNode = (point) => {
+      if(point && point.data > data) {
+        point.left = addNode(point.left)
+        //console.log('left');
+      } else if (point && point.data < data) {
+        point.right = addNode(point.right)
+       //console.log('right');
+      } else {
+        point = new Node(data);
+       //console.log('NEW', data);
+      }
+      return point;
+    }
+    
+    this._root = addNode(this.root());
+  }
+
+  showTree( node ) {
+    if ( !node ) {
+      console.log('---------');
+      return;
+    }
+    console.log(node, 'left', node && node.left);
+    console.log(node, 'right', node && node.right);
+    this.showTree(node && node.left);
+    this.showTree(node && node.right);
   }
 
   has(/* data */) {
@@ -23,9 +56,26 @@ class BinarySearchTree {
     // remove line with error and write your code here
   }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
+  find( data ) {
+    //throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    console.log('find data:', data);
+    const findNode = (point) => {
+      if(point && point.data > data) {
+        //console.log(point.data, 'point && point.data > data', (point && point.data > data) && 'left');
+        return findNode(point.left)
+      } else if (point && point.data < data) {
+        //console.log(point.data, 'point && point.data < data', (point && point.data < data) && 'right');
+        return findNode(point.right)
+      } else if (point && point.data === data) {
+        //console.log(point.data, point instanceof Node, 'point && point.data === data', (point && point.data === data) && point);
+        return point;  
+      } else {
+        //console.log('NEW');
+        return null //new Node(null);
+      }
+    }
+    return findNode(this.root());
   }
 
   remove(/* data */) {
@@ -44,6 +94,22 @@ class BinarySearchTree {
   }
 }
 
+
 module.exports = {
   BinarySearchTree
 };
+
+const tree = new BinarySearchTree();
+      tree.add(2);
+      tree.add(7);
+      tree.add(1);
+      tree.add(8);
+      tree.add(4);
+      tree.add(32);
+      tree.add(12);
+      tree.add(14);
+      tree.showTree(tree.root());
+      console.log(tree.find(2).data, 2);
+      console.log(tree.find(8).data, 8);
+      console.log(tree.find(14).data, 14);
+      console.log(tree.find(32).data, 32);
